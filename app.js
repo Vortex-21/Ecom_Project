47c2fd0db0345c3311cc38b6e0c46dd31fc4d90e
Anshuman Laskar <anshumanlaskar1@gmail.com>
2024-02-12 14:52:24 +0530

    updated!

--- app.js
+++ app.js
@@ -1,50 +1,54 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Listing = require("./models/listings");
-const ejs = require('ejs');
-const path = require('path');
-const methodOverride = require('method-override');
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'));
+const ejs = require("ejs");
+const path = require("path");
+const methodOverride = require("method-override");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-
-let methodOverride = require('method-override')
-
- 
+app.use(express.urlencoded({ extended: true }));
 // override with POST having ?_method=DELETE
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"));
 
-//Edit Listing Route
-app.put("/listings/:id/updateData",async (req,res)=>{
-    let newData = req.body.listing;
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id,
-        {
-            name:newData.name,
-            price:newData.price,
-            description:newData.description
-        });
-    res.send("Updated!");
-});
-app.get("/listings/:id/editListing",async(req,res)=>{
+
+//Show Listing Route
+app.get("/listings/:id/show",async(req,res)=>{
     let {id} = req.params;
     let listing = await Listing.findById(id);
-    res.render("listings/editListing.ejs",{listing});
+    res.render("listings/showListing.ejs",{listing});
+});
+
+//Edit Listing Route
+app.put("/listings/:id/updateData", async (req, res) => {
+  let newData = req.body.listing;
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(
+    id, 
+    { ...newData }, 
+    { runValidators: true 
+  });
+  res.send("Updated!");
+});
+
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  let listing = await Listing.findById(id);
+  res.render("listings/editListing.ejs", { listing });
 });
 
 //All Listings route
-app.get("/listings",async(req,res)=>{
-    let allListings = await Listing.find();
-    res.render('listings/allListings.ejs',{allListings});
-    // res.send("All Listings Route");
+app.get("/listings", async (req, res) => {
+  let allListings = await Listing.find();
+  res.render("listings/allListings.ejs", { allListings });
+  // res.send("All Listings Route");
 });
 
-app.get("/",(req,res)=>{
-    res.send("Working!");
+app.get("/", (req, res) => {
+  res.send("Working!");
 });
 
-
-app.listen(3000,(req,res)=>{
-    console.log("Listening at port 3000");
+app.listen(3000, (req, res) => {
+  console.log("Listening at port 3000");
 });
